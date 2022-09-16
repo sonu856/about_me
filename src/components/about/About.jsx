0bc250ff7e49111289bdd378f71496f79eff102b
sonu kumar <sonukumar@iitbhilai.ac.in>
2022-09-16 08:06:40 +0530

    updated cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -21,17 +21,17 @@ const About = () => {
               <article className='about__card'>
                 <FaAward className='about__icon'/>
                 <h5>Experience</h5>
-                <small>3+ Years Working</small>
+                <small>Fresher</small>
               </article>
               <article className='about__card'>
                 <FiUsers className='about__icon'/>
                 <h5>Clients</h5>
-                <small>200+ Worldwide</small>
+                <small>NA</small>
               </article>
               <article className='about__card'>
                 <VscFolderLibrary className='about__icon'/>
                 <h5>Projects</h5>
-                <small>80+ Completed</small>
+                <small>3 Completed</small>
               </article>
             </div>
             <p>
